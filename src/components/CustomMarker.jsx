339b70f0f8686f67be673fd5a329fe177d0e3e70
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-07 00:54:43 +0530

    update: added custom marker

---
+++ src/components/CustomMarker.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useLocationContext } from "../context/LocationContext";
+import { GET_ICON } from "../services/apiUrls";
+
+const CustomMarker = () => {
+  const { weatherDetails } = useLocationContext();
+  return (
+    <div className="h-16 w-16 p-2 border rounded-md z-30 absolute bg-sky-200 flex flex-col gap-1 items-center">
+      <img
+        className="h-7 w-7"
+        src={`${GET_ICON}${weatherDetails?.icon}.png`}
+        alt=""
+      />
+      <span className="text-sm font-semibold ">
+        {weatherDetails?.description}
+      </span>
+    </div>
+  );
+};
+
+export default CustomMarker;
