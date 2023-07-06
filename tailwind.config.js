ddda8cd529f2534e6ce657013d626ee3095be105
SandhyaR1007 <grajwanshi1008@gmail.com>
2023-07-07 01:23:34 +0530

    update: add xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "375px",
+        ...defaultTheme.screens,
+      },
+    },
   },
   plugins: [],
 };
